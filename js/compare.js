5cb67667c404fbae2078e245f62ba08918e216d2
xinglinb <986294216@qq.com>
2017-05-06 12:24:54 +0800

    a

--- js/compare.js
+++ js/compare.js
@@ -2,44 +2,55 @@
 window.onload=function(){
 
 	$(".sub").click(function(){
-		$("#add").show();
+		
+		$("#left-car").slideDown();
 	});
 
-	$("#add li").click(function(){
-		var x=1;
-		for (var i = 0; i < $("#add li").length; i++) {
-			if ($("#add ul").find("li").eq(i).attr("index")!=0) {
-				x=x+1
-			};
+	$("#left-car li").click(function(){
+		var x=0;
+		// for (var i = 0; i < $("#left-car li").length; i++) {
+		// 	if ($("#left-car ul").find("li").eq(i).attr("index")==0) {
+		// 		x=x+1
+		// 	};
 			
-		};
-		if (x==1) {
-			$(this).css({"background":"#bcbaba"});
-			$(this).attr("index","1");
-		}
-		else if(x==2){
-			$(this).css({"background":"#bcbaba"});
-			$(this).attr("index","2");
-		}
-		else if(x==3){
-			for (var i = 0; i < $("#add li").length; i++) {
-				if ($("#add ul").find("li").eq(i).attr("index")==1) {
-					$("#add ul").find("li").eq(i).attr("index","0");
-					$("#add ul").find("li").eq(i).css({"background":"#c8c8d6"})
+		// };
+		if ($(this).attr("index")==0) {
+			for (var i = 0; i < $("#left-car li").length; i++) {
+				if ($("#left-car ul").find("li").eq(i).attr("index")==1) {
+					$("#left-car ul").find("li").eq(i).attr("index","2");
+
 				}
-				else if ($("#add ul").find("li").eq(i).attr("index")==2) {
-					$("#add ul").find("li").eq(i).attr("index","1");
+				else if ($("#left-car ul").find("li").eq(i).attr("index")==2) {
+					$("#left-car ul").find("li").eq(i).attr("index","0");
+					$("#left-car ul").find("li").eq(i).find("p[class='li-add']").text("+")
 				};
 			};
-			$(this).css({"background":"#bcbaba"});
-			$(this).attr("index","2");
+			$(this).attr("index","1");
+			$(this).find("p[class='li-add']").text("-")
+		}
+		else if($(this).attr("index")==1){
+			$(this).attr("index","0");
+			$(this).find("p[class='li-add']").text("+")
+		}
+		else if($(this).attr("index")==2){
+			
+			$(this).attr("index","0");
+			$(this).find("p[class='li-add']").text("+")
 		};
 
 		
 	});
 
-	$("#add-b").click(function(){
-		$("#add").hide()
+	$("#left-car-b").click(function(){
+		// $("#left-car").hide()
+		for (var i = 0; i < $("#left-car li").length; i++) {
+				if ($("#left-car ul").find("li").eq(i).attr("index")==1) {
+					var x1=$("#left-car ul").find("li").eq(i).find("p[class!='li-add']").text()
+				}
+				else if ($("#left-car ul").find("li").eq(i).attr("index")==2) {
+					var x2=$("#left-car ul").find("li").eq(i).find("p[class!='li-add']").text()
+				};
+			};
 	});
 	
 }
